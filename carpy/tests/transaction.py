c566e328294991c9467c908f046c249a9f2e7447
Matic Žgur <matic.zgur@zemanta.com>
2014-05-27 19:50:59 +0200

    Added transaction module.

---
+++ carpy/tests/transaction.py
@@ -0,0 +1,92 @@
+import gc
+import sys
+import threading
+import time
+from unittest import TestCase
+
+import carpy
+import carpy.transaction
+
+try:
+	import mock
+except ImportError:
+	import unittest.mock as mock
+
+
+class TranscationTest(TestCase):
+
+	@mock.patch('carpy.config')
+	@mock.patch('carpy.transaction.time')
+	def test_transcation(self, time_mock, config_mock):
+		config_mock.dict.in_dict = {'APP_NAME': 'Test App'}
+
+		time_start = time.time()
+		time_mock.time.return_value = time_start
+
+		transaction = carpy.transaction.Transaction()
+		self.assertEqual(transaction.app_name, config_mock['APP_NAME'])
+		self.assertEqual(transaction.start_time, 0.0)
+		self.assertEqual(transaction.duration, 0.0)
+
+		transaction.__enter__()
+		self.assertEqual(transaction.start_time, time_start)
+		self.assertIs(carpy.transaction.transactions_cache[carpy.transaction.get_thread_id()], transaction)
+
+		time_end = time.time() + 5
+		time_mock.time.return_value = time_end
+
+		transaction.__exit__()
+		self.assertEqual(transaction.duration, time_end - time_start)
+
+		del transaction
+		gc.collect()
+		self.assertTrue(carpy.transaction.get_thread_id() not in carpy.transaction.transactions_cache)
+		self.assertIs(carpy.transaction.transactions_cache.get(carpy.transaction.get_thread_id()), None)
+
+	@mock.patch('carpy.config')
+	@mock.patch('carpy.transaction.time')
+	def test_child_transaction(self, time_mock, config_mock):
+		parent_transaction = carpy.transaction.Transaction().__enter__()
+		child_transaction = carpy.transaction.Transaction(parent=parent_transaction).__enter__()
+
+		self.assertIs(parent_transaction.children[0], child_transaction)
+		self.assertIs(child_transaction.parent, parent_transaction)
+
+	def test_get_thread_id(self):
+		expected_thread_id = threading.current_thread().ident
+		self.assertEqual(carpy.transaction.get_thread_id(), expected_thread_id)
+
+	def test_get_greenlet_id(self):
+		# obj = object()
+		# greenlet_mock.getcurrent.return_value = obj()
+		# greenlet_mock.parent = None
+		return
+
+		class FakeObject:
+			parent = None
+
+		self.greenlet_obj = FakeObject()
+
+		class FakeGreenlet(object):
+			@staticmethod
+			def getcurrent():
+				return self.greenlet_obj
+
+		self.original_greenlet = sys.modules.get('greenlet')
+		sys.modules['greenlet'] = FakeGreenlet
+
+		self.assertIs(carpy.transaction.get_thread_id(), id(self.greenlet_obj))
+
+		if self.original_greenlet:
+			sys.modules['greenlet'] = self.original_greenlet
+		else:
+			del sys.modules['greenlet']
+
+	@mock.patch('carpy.config')
+	def test_get_transaction(self, config_mock):
+		config_mock.dict.in_dict = {'APP_NAME': 'Test App'}
+
+		transaction = carpy.transaction.Transaction()
+		transaction.__enter__()
+
+		self.assertIs(carpy.transaction.get_transaction(), transaction)
---
+++ carpy/transaction.py
@@ -0,0 +1,57 @@
+import sys
+import threading
+import time
+import weakref
+
+import carpy
+
+transactions_cache = weakref.WeakValueDictionary()
+
+
+class Transaction(object):
+	def __init__(self, parent=None):
+		self.app_name = carpy.config['APP_NAME']
+
+		self.start_time = 0.0
+		self.duration = 0.0
+
+		self.parent = parent
+
+		self.children = []
+
+	def add_child(self, transaction):
+		self.children.append(transaction)
+
+	def __enter__(self):
+		self.start_time = time.time()
+
+		if self.parent is not None:
+			self.parent.add_child(self)
+		else:
+			transactions_cache[get_thread_id()] = self
+
+		return self
+
+	def __exit__(self):
+		self.duration = time.time() - self.start_time
+
+
+def get_thread_id():
+	''' Returns the thread ID of the current thread or greenlet ID if running
+	in greenlet.
+	'''
+	greenlet = sys.modules.get('greenlet')
+	if greenlet:
+		current_greenlet = greenlet.getcurrent()
+		print("bafdgfgfg#")
+		if current_greenlet is not None and current_greenlet.parent:
+			return id(current_greenlet)
+
+	return threading.current_thread().ident
+
+
+def get_transaction():
+	''' Returns current transation from cache.
+	'''
+
+	return transactions_cache.get(get_thread_id())
